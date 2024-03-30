ea30d9ec231e2f52a6696f625f0c644107a4870d
qiujerry <qiujer5687@gmail.com>
2024-03-30 00:26:57 -0400

    start of simple trading bot

---
+++ simple_bot.py
@@ -0,0 +1,28 @@
+class SimpleBot:
+    def __init__(self,cash):
+        self.cash = cash
+        self.days = 0
+        self.d_stock=0
+        self.oil=0
+        self.d_history = []
+        self.c_history = []
+    
+    def next_day(self, coil,dstock):
+        if self.days == 0:
+            #first day 75% delta, 25% crude oil
+            self.d_stock = (self.cash*.75) / float(dstock[1])
+            self.oil = (self.cash*.25) / float(coil[1])
+            self.cash=0
+        
+            
+        
+        self.days = self.days+1
+        if(self.days%365==0):
+            self.cash = self.cash + (self.cash*.0527)
+        
+        self.d_history.append(dstock)
+        self.c_history.append(coil)
+
+    def cur_val(self):
+        total = self.cash + self.d_stock*float(self.d_history[-1][4]) + self.oil*float(self.c_history[-1][4])
+        return total
---
+++ simulator.py
@@ -0,0 +1,29 @@
+import csv
+from simple_bot import SimpleBot
+
+def main():
+    print("Hello World")
+
+    bot = SimpleBot(1000)
+
+    with open('DAL_data_1.csv', newline='') as f:
+        reader = csv.reader(f)
+        delta_data = list(reader)
+
+    print(len(delta_data))
+    
+
+    with open('CLF_data_1.csv', newline='') as z:
+        reader = csv.reader(z)
+        CLF_data = list(reader)
+    
+    print(len(CLF_data))
+
+    for x in range(1,len(CLF_data)):
+        bot.next_day(CLF_data[x],delta_data[x])
+
+    print(bot.cur_val())
+
+
+if __name__ == "__main__":
+    main()
